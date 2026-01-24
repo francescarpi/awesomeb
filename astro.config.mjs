5c019d0b68cee805fa7190727632a854bce1cd24
Francesc Arpi Roca <francesc.arpi@gmail.com>
2026-01-24 18:39:11 +0100

    feat: add vite

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,49 @@
 import { defineConfig } from 'astro/config';
 import icon from 'astro-icon'
 import tailwindcss from "@tailwindcss/vite";
+import electron from "astro-electron";
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+import path from 'path'
+
+const commonAliases = {
+  "@": path.resolve("src/main"),
+  "~": path.resolve("src/shared")
+}
 
 export default defineConfig({
-  integrations: [icon({
-    iconDir: 'src/renderer/icons',
-  })],
+  integrations: [
+    icon({
+      iconDir: 'src/renderer/icons',
+    }),
+    electron({
+      main: {
+        entry: 'src/main/index.ts',
+        vite: {
+          build: {
+            outDir: 'dist-electron/main',
+          },
+          resolve: {
+            alias: commonAliases,
+          }
+        },
+      },
+      preload: {
+        vite: {
+          build: {
+            minify: 'esbuild',
+            rollupOptions: {
+              input: {
+                'preload/browser': 'src/preload/browser.ts',
+              },
+            },
+          },
+          resolve: {
+            alias: commonAliases,
+          }
+        }
+      },
+      renderer: {}
+    })],
   output: 'static',
   srcDir: 'src/renderer',
   outDir: 'dist/renderer',
@@ -13,11 +51,21 @@ export default defineConfig({
     assetsPrefix: '../',
   },
   vite: {
-    plugins: [tailwindcss()],
+    plugins: [tailwindcss(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'src/main/assets/icons',
+          dest: '.',
+        },
+      ],
+    }),
+    ],
     resolve: {
       alias: {
-        "@main/*": ["./src/main/*"],
-        "@shared/*": ["./src/shared/*"]
+        ...commonAliases,
+        "@preload": "./src/preload",
+        "@renderer": "./src/renderer",
       }
     }
   },
