9f1f1f420f563ad1b516aaf3fe22f50b962237c8
Francesc Arpi Roca <francesc.arpi@gmail.com>
2026-02-09 14:30:01 +0100

    refactor: clean unnecessary code

--- src/main/core/commands/tab-select.ts
+++ src/main/core/commands/tab-select.ts
@@ -1,8 +1,5 @@
 import { TTabId } from '~/types';
 import { ICommand } from './types';
-import log from 'electron-log';
-
-const scopeLog = log.scope('SelectTabCommand');
 
 export interface ICommandParams {
   tabId: TTabId;
@@ -15,13 +12,7 @@ export const Command: ICommand<ICommandParams> = {
   name: 'Select Tab',
   description: 'Selects a tab by its ID in the focused window.',
   visibility: ({ tab }) => !!tab,
-  async handler({ browser, window, params }) {
-    const targetTab = browser.getTab(params.tabId);
-    if (!targetTab) {
-      scopeLog.warn(`Tab with ID ${params.tabId} not found.`);
-      return;
-    }
-
+  async handler({ window, params }) {
     await window.selectTab(params.tabId);
   },
 };
