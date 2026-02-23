ddd0716bd493f23a9e718137e1cbac2c3fa3fa96
Francesc Arpi Roca <francesc.arpi@gmail.com>
2026-02-23 08:49:57 +0100

    feat: add session events log

--- src/main/core/browser/events.session.ts
+++ src/main/core/browser/events.session.ts
@@ -2,9 +2,14 @@ import { Browser } from '@/core';
 import { TPartitionId } from '~/types';
 import { session, desktopCapturer } from 'electron';
 import { sanitizeUserAgent } from '@/utils';
+import log from 'electron-log';
+
+const scopeLog = log.scope('SessionEvents');
 
 export function registerSessionEvents(_browser: Browser, partitionId: TPartitionId) {
   const ses = session.fromPartition(partitionId);
+
+  scopeLog.info(`Registering session events for partition: ${partitionId}`);
 
   // ----------------------------------------------------------------------------------------------- //
   ses.setDisplayMediaRequestHandler(
@@ -22,12 +27,14 @@ export function registerSessionEvents(_browser: Browser, partitionId: TPartition
     const uaKey = Object.keys(newHeaders).find((key) => key.toLowerCase() === 'user-agent');
 
     if (!uaKey) {
+      scopeLog.warn('User-Agent header not found in request headers:', details.requestHeaders);
       callback({});
       return;
     }
 
     const userAgent = newHeaders[uaKey];
     if (!userAgent) {
+      scopeLog.warn('User-Agent header is empty:', details.requestHeaders);
       callback({});
       return;
     }
