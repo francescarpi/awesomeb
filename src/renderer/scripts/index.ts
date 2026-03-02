26d853b867ebb2c00d4e637fbcd893b6af2ade6b
Francesc Arpi Roca <francesc.arpi@gmail.com>
2026-03-02 12:44:20 +0100

    refactor: change renderer imports

--- src/renderer/scripts/index.ts
+++ src/renderer/scripts/index.ts
@@ -1,3 +1,7 @@
 export { listWithSearchManager } from './list-with-search';
 export { paginatorManager } from './paginator';
 export { getSearchParams } from './url';
+export { inputManager, type TSetValue, type TSetDefault } from './inputs';
+export { dialogConfirm } from './dialog-confirm';
+export { dialogWithSelect, type IOption } from './dialog-with-select';
+export { onClick } from './buttons';
