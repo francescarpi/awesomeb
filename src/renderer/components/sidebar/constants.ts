057b571c3419591b5a80c66be73472b468747950
Francesc Arpi Roca <francesc.arpi@gmail.com>
2026-04-23 19:46:35 +0200

    a

---
+++ src/renderer/components/sidebar/constants.ts
@@ -0,0 +1,66 @@
+export const DESKTOP_CONTAINER_CLASSES = [
+  'group',
+  'relative',
+  'hidden',
+  'sidebar:block',
+  '[.selected]:block',
+];
+
+export const DESKTOP_ELEMENT_CLASSES = [
+  'border',
+  'rounded-full',
+  'sidebar:w-9',
+  'sidebar:h-9',
+  'sidebar:text-sm',
+  'w-7',
+  'h-7',
+  'text-xs',
+  'cursor-pointer',
+  'sidebar:flex',
+  'items-center',
+  'justify-center',
+  'flex',
+];
+
+export const INDICATOR_CLASSES = [
+  'w-[3px]',
+  'h-[3px]',
+  'rounded-full',
+  'absolute',
+  'bottom-1',
+  'left-1/2',
+  '-translate-x-1/2',
+];
+
+export const ATTENTION_CLASSES = [
+  'w-2',
+  'h-2',
+  'bg-red-500',
+  'rounded-full',
+  'absolute',
+  'right-0',
+  'top-0',
+];
+
+export const ATTENTION_PING_CLASSES = [
+  'w-3',
+  'h-3',
+  'bg-red-500',
+  'rounded-full',
+  'animate-ping',
+  'absolute',
+  '-right-0.5',
+  '-top-0.5',
+];
+
+export const DESKTOP_NAME_CLASSES = [
+  'text-[10px]',
+  'absolute',
+  'mt-1',
+  'whitespace-nowrap',
+  'left-1/2',
+  '-translate-x-1/2',
+  'sidebar:hidden',
+  'sidebar:group-[.selected]:block',
+  'sidebar:group-hover:block',
+];
