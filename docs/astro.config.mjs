b9a242d836852bd2d08ad1fc6bbfdc7a549d32d1
Francesc Arpí Roca <francescarpi@users.noreply.github.com>
2026-07-29 21:46:59 +0200

    docs: add themes configuration page in ca, es, and en (#226)

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -56,6 +56,7 @@ export default defineConfig({
           items: [
             { label: 'General', translations: { 'ca': 'General', 'es': 'General' }, slug: 'config/general' },
             { label: 'Profiles', translations: { 'ca': 'Perfils', 'es': 'Perfils' }, slug: 'config/profiles' },
+            { label: 'Themes', translations: { 'ca': 'Temes', 'es': 'Temas' }, slug: 'config/themes' },
           ]
         }
       ],
