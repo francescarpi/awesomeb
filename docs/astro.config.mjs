a2bc5fc7529ce86d908bc355bb910b2a9d0f200c
Francesc Arpi Roca <francesc.arpi@gmail.com>
2026-08-05 10:32:09 +0200

    docs(tabs): add rename tab page in ca, es, and en

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -70,6 +70,7 @@ export default defineConfig({
             { label: 'Close', translations: { 'ca': 'Tancar', 'es': 'Cerrar' }, slug: 'tabs/close' },
             { label: 'Reopen', translations: { 'ca': 'Reobrir', 'es': 'Reabrir' }, slug: 'tabs/closed' },
             { label: 'Navigate', translations: { 'ca': 'Navegar', 'es': 'Navegar' }, slug: 'tabs/navigate' },
+            { label: 'Rename', translations: { 'ca': 'Renombrar', 'es': 'Renombrar' }, slug: 'tabs/rename' },
           ]
         },
       ]
